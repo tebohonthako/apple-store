9b3c70da6b84810a1ef6580e3e48f7e4e8a8f054
John <slippersmaloney@gmail.com>
2023-08-03 15:51:29 +0200

    Home Page cards pull through with correct data

---
+++ src/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import products from './data';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Welcome to our E-Commerce Store</h1>
+      <div className="row product-list">
+        {products.map((product) => (
+          <div key={product.id} className="col-md-4 mb-4">
+            <div className="card" style={{ width: '18rem' }}>
+              <img src={product.imageUrl} className="card-img-top" alt={product.title} />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">{product.description}</p>
+                <p>Price: {product.currency} {product.price.toFixed(2)}</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
