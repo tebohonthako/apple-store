2fa5bfd565cac15db6993eeee3c9299bf004afb8
T E B O O <tjaynthako@gmail.com>
2023-08-09 11:44:01 +0200

    added box shadow to search bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import ProductDetails from './ProductDetails';
+import './App.css';
 
 const App = () => {
   return (
--- src/HomePage.js
+++ src/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import products from './data';
 import SideBar from './SideBar';
+import './App.css';
+
 
 const Homepage = () => {
   return (
