130e04bcbc3d3ad174b7322a7d9389fd19296420
saajida mathen <mathensaajida@gmail.com>
2023-08-11 09:09:21 +0200

    totals now correctly appear on the Bag.js and BagPage.js

--- src/BagContext.js
+++ src/BagContext.js
@@ -1,21 +1,38 @@
-import React, { createContext, useState, useContext } from 'react';
+// BagContext.js
+
+import React, { createContext, useContext, useState } from 'react';
 
 const BagContext = createContext();
 
 export const BagProvider = ({ children }) => {
   const [bagItems, setBagItems] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const addToBag = (product) => {
-    setBagItems([...bagItems, product]);
+    setBagItems([...bagItems, { ...product, quantity: 1 }]); // Set initial quantity to 1
+    setTotal(total + product.price); // Update the total
   };
 
   const removeFromBag = (product) => {
     const updatedBagItems = bagItems.filter(item => item !== product);
     setBagItems(updatedBagItems);
+    setTotal(total - product.price * product.quantity);
+  };
+
+  const updateQuantity = (product, newQuantity) => {
+    const updatedBagItems = bagItems.map(item =>
+      item === product ? { ...item, quantity: newQuantity } : item
+    );
+
+    const updatedTotal = updatedBagItems.reduce((sum, item) =>
+      sum + item.price * item.quantity, 0);
+
+    setBagItems(updatedBagItems);
+    setTotal(updatedTotal);
   };
 
   return (
-    <BagContext.Provider value={{ bagItems, addToBag, removeFromBag }}> {/* Add removeFromBag */}
+    <BagContext.Provider value={{ bagItems, addToBag, removeFromBag, updateQuantity, total }}>
       {children}
     </BagContext.Provider>
   );
