c4a5fc18a7b2cc8a2aba05937d58c50abb19a23a
saajida mathen <mathensaajida@gmail.com>
2023-08-14 00:15:23 +0200

    success page styled

---
+++ src/OrderSuccess.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './SuccessPage.css'; // Create a new CSS file for your SuccessPage and import it here
+
+const SuccessPage = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          <span>O</span>
+          <span>R</span>
+          <span>D</span>
+          <span>E</span>
+          <span>R</span>
+          <br />
+          <span>P</span>
+          <span>L</span>
+          <span>A</span>
+          <span>C</span>
+          <span>E</span>
+          <span>D</span>
+          <br />
+          <span>S</span>
+          <span>U</span>
+          <span>C</span>
+          <span>C</span>
+          <span>E</span>
+          <span>S</span>
+          <span>S</span>
+          <span>F</span>
+          <span>U</span>
+          <span>L</span>
+          <span>L</span>
+          <span>Y</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessPage;
